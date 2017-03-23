d92783e8b6afbf79b59ab380c9b9a2cd9d31edeb
Josiah Maius S. Ebia <josiah_ebia@yahoo.com>
2017-03-23 09:43:11 +0800

    Added partitioning banner

--- Main.java
+++ Main.java
@@ -30,8 +30,17 @@ public class Main {
 	// method that simulates memory partioning
 	private static void partitioning() {
 		
-		// declare variables
+		System.out.print("______               _    _  _    _                _               "+
+		+"| ___ \             | |  (_)| |  (_)              (_)              "+
+		+"| |_/ /  __ _  _ __ | |_  _ | |_  _   ___   _ __   _  _ __    __ _ "+
+		+"|  __/  / _` || '__|| __|| || __|| | / _ \ | '_ \ | || '_ \  / _` |"+
+		+"| |    | (_| || |   | |_ | || |_ | || (_) || | | || || | | || (_| |"+
+		+"\_|     \__,_||_|    \__||_| \__||_| \___/ |_| |_||_||_| |_| \__, |"+
+		+"                                                              __/ |"+
+		+"                                                             |___/"
+		);
 		
+		// declare variables
 		int[] memories;
 		int[] partitions;
 		int[] allocatedMemory;
