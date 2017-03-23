46dee5a470ee145df8cdfc238bfd06053ba8ed96
Josiah Maius S. Ebia <josiah_ebia@yahoo.com>
2017-03-23 09:46:24 +0800

    removed partitioning banner

--- Main.java
+++ Main.java
@@ -30,16 +30,6 @@ public class Main {
 	// method that simulates memory partioning
 	private static void partitioning() {
 		
-		System.out.print("______               _    _  _    _                _               "+
-		+"| ___ \             | |  (_)| |  (_)              (_)              "+
-		+"| |_/ /  __ _  _ __ | |_  _ | |_  _   ___   _ __   _  _ __    __ _ "+
-		+"|  __/  / _` || '__|| __|| || __|| | / _ \ | '_ \ | || '_ \  / _` |"+
-		+"| |    | (_| || |   | |_ | || |_ | || (_) || | | || || | | || (_| |"+
-		+"\_|     \__,_||_|    \__||_| \__||_| \___/ |_| |_||_||_| |_| \__, |"+
-		+"                                                              __/ |"+
-		+"                                                             |___/"
-		);
-		
 		// declare variables
 		int[] memories;
 		int[] partitions;
